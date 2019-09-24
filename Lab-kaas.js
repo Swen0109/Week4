8010c2ceb9553de1bb854851e16a991d0d71d89d
Swen <99058148@mydavinci.nl>
2019-09-24 11:39:44 +0200

    Commit 1
    Map aangemaakt met de opdrachten.

---
+++ Lab-kaas.js
@@ -0,0 +1,19 @@
+
+var GeleKaas =prompt ('Is de kaas geel?')
+if (GeleKaas >= 'ja'){
+	var Kaasmetgaten = prompt ('Zitten er gaten in?')
+}
+	if (Kaasmetgaten == 'ja'){
+		var DureKaas = prompt('Is de kaas belachelijk duur?')
+	}
+	if (DureKaas == 'ja'){
+		var WelkeKaas = prompt ('Welke kaas is het?')
+	}
+		if (WelkeKaas == 'Emmenthaler'){
+		document.write ('Gefeliciteerd u heeft het goed!')
+	}
+	    else {
+	    document.write ('Sorry, probeer het opnieuw!')
+
+
+}
